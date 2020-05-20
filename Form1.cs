5484123ecb14ff31680506a2747f5f6844bfff5d
eray <eray_hamurlu@hotmail.com>
2020-05-20 13:16:29 +0300

    upload

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Facade01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string ad; // Çektiğimiz verileri tutacak değişkenleri tanımlıyoruz
+        private string tc;
+        private string musterino;
+        private string talep;
+        private void KrediCek(object sender, EventArgs e)
+        {
+            ad = textBox1.Text; // textBox'dan verileri çekiyoruz
+            tc = textBox2.Text;
+            musterino = textBox3.Text;
+            talep = textBox4.Text;
+
+            Facade fcd = new Facade(); // Facade sınıfından nesnemizi gerçekliyoruz
+            label1.Text = fcd.KrediKullan( // Musteri bilgilerini yolluyoruz 
+                new Musteri { Ad = ad, TcNo = tc, MusteriNumarasi = musterino }
+                , talep + " TL");
+        }
+    }
+}
